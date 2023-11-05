0c6c894af4d955e9e03d52e68c7d0ceb9a955368
Olive <141564336+xxOliveXyzxx@users.noreply.github.com>
2023-11-05 14:39:55 -0700

    Add files via upload

---
+++ js/brews.js
@@ -0,0 +1,16 @@
+
+///////////////////////////////////
+//        BOBA VARIABLES         //
+///////////////////////////////////
+var boba = 0;
+var rainbowjelly = 0;
+var coffeejelly = 0;
+var poppingred = 0;
+var poppingorange = 0;
+var poppingyellow = 0;
+var poppinggreen = 0;
+var poppingblue = 0;
+var crystal = 0;
+var lychee = 0;
+
+
---
+++ js/game.js
@@ -0,0 +1,18 @@
+function newdrink() {
+	    document.getElementById("sellbt").style.display = "block";
+		document.getElementById("newbt").style.display = "none";
+
+}
+
+function order() {
+	bobacount.innerHTML = boba;
+	raincount.innerHTML = rainbowjelly;
+	coffeecount.innerHTML = coffeejelly;
+	redcount.innerHTML = poppingred;
+	orangecount.innerHTML = poppingorange;
+	yellowcount.innerHTML = poppingyellow;
+	greencount.innerHTML = poppinggreen;
+	bluecount.innerHTML = poppingblue;
+    crystalcount.innerHTML = crystal;
+    lycheecount.innerHTML = lychee;
+}
